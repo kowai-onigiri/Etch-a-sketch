60bdaf473070485be729717227a5d111d777a986
kowai-onigiri <alexischambers627@gmail.com>
2022-03-25 20:39:40 -0400

    update script

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// 16x16 grid
+const container = document.querySelector('#container');
+
+const square = document.createElement('div');
+square.classList.add('square');
+square.textContent = ' ';
+square.setAttribute('style', 'background: white; border: 1px solid black; width: 50px; height: 50px');
+
+
+container.appendChild(square);
+
 
